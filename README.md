35f751b3c3d6f2dadd5c90d58168664538b861ca
Magdalena Maislinger <magdalena.maislinger@vivid-planet.com>
2023-12-05 06:33:06 +0100

    Day 4

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: 12.316ms
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.242ms
+Time part 2: 1.071ms
+Both parts: 3.313ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 19.705ms
+Total stars: 8/50
+Total time: 23.018ms
 ```
 
 <!--/RESULTS-->
---
+++ dist/day04/index.js
@@ -0,0 +1,82 @@
+import run from "aocrunner";
+const parseInput = (rawInput) => rawInput;
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const cardsInput = input.split("\n");
+  const cards = [];
+  let result = 0;
+  cardsInput.forEach((card) => {
+    const cardNumbers = card.substring(card.indexOf(":") + 1).split(" | ").map((item) => item.trim());
+    cards.push({
+      winningNumbers: cardNumbers[0].split(/ +/).filter((item) => item !== " ").map((num) => parseInt(num)),
+      myNumbers: cardNumbers[1].split(/ +/).filter((item) => item !== " ").map((num) => parseInt(num))
+    });
+  });
+  cards.forEach((card) => {
+    const matchingNumbers = card.winningNumbers.filter((value) => card.myNumbers.includes(value));
+    if (rawInput.includes("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")) {
+      console.log(matchingNumbers.length);
+    }
+    result += matchingNumbers.reduce((acc, _current) => {
+      return acc === 0 ? 1 : acc * 2;
+    }, 0);
+  });
+  return result;
+};
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const cardsInput = input.split("\n");
+  const cards = [];
+  let result = 0;
+  cardsInput.forEach((card) => {
+    const cardNumbers = card.substring(card.indexOf(":") + 1).split(" | ").map((item) => item.trim());
+    cards.push({
+      winningNumbers: cardNumbers[0].split(/ +/).filter((item) => item !== " ").map((num) => parseInt(num)),
+      myNumbers: cardNumbers[1].split(/ +/).filter((item) => item !== " ").map((num) => parseInt(num))
+    });
+  });
+  const copies = Array(cardsInput.length).fill(1);
+  cards.forEach((card, index) => {
+    const matchingNumbers = card.winningNumbers.filter((value) => card.myNumbers.includes(value));
+    for (let i = 1; i <= matchingNumbers.length; i++) {
+      copies[index + i] = copies[index + i] + copies[index];
+    }
+  });
+  result = copies.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
+  return result;
+};
+run({
+  part1: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13
+      }
+    ],
+    solution: part1
+  },
+  part2: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30
+      }
+    ],
+    solution: part2
+  },
+  trimTestInputs: true,
+  onlyTests: false
+});
+//# sourceMappingURL=index.js.map
